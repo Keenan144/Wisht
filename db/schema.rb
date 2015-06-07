969a61dd068136a606fd8278f8aa067f6488b583
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-06-08 00:58:09 +0800

    migrateion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607132846) do
+ActiveRecord::Schema.define(version: 20150607165417) do
+
+  create_table "lists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
