795cf5c22653516708da2b6c407e657b0a7d5a50
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-07-15 03:26:31 +0800

    icons not working

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'nokogiri'
-require 'open-uri'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Wisht
   class Application < Rails::Application
+    require 'nokogiri'
+    require 'open-uri'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
